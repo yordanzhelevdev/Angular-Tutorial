b5d0e854fbde888cca8706e1b116ccb7dc7c85b8
yordanzhelevdev <yordanzhelevdev@gmail.com>
2018-09-05 16:14:33 +0300

    Completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'heroes', component: HeroesComponent },
