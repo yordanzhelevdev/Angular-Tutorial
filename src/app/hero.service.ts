0592aa204b5ec93f36f1e283a25ba725a76f3b30
yordanzhelevdev <yordanzhelevdev@gmail.com>
2018-09-05 10:54:06 +0300

    Routes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,8 +10,14 @@ import { MessageService } from './message.service';
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
+    this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    //TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
   constructor(private messageService: MessageService) {}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
-import { HeroService } from '../hero.service';
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: "app-heroes",
@@ -8,18 +8,12 @@ import { HeroService } from '../hero.service';
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[];
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 
-  heroes: Hero[];
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
